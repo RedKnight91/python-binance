1f70a3de2b3dc0be1e54384097ab9c5bd8d9239d
Michael Cazzarolli <michaelcazzarolli@gmail.com>
2021-06-18 22:46:57 +0200

    Update _historical_klines() function
    The function should get the exact amount of candles set in the 'limit' parameter.
    Currently it does not, so this change fixes that.
    Given that the max batch is 1000 candles, higher limits get filled in more batches.

--- binance/client.py
+++ binance/client.py
@@ -931,83 +931,72 @@ class Client(BaseClient):
         """
         return self._historical_klines(symbol, interval, start_str, end_str=end_str, limit=limit, klines_type=klines_type)
 
-    def _historical_klines(self, symbol, interval, start_str, end_str=None, limit=500,
-                           klines_type: HistoricalKlinesType = HistoricalKlinesType.SPOT):
-        """Get Historical Klines from Binance (spot or futures)
+	def _historical_klines(self, symbol, interval, start_str, end_str=None, limit=500,
+						   klines_type: HistoricalKlinesType = HistoricalKlinesType.SPOT):
+		"""Get Historical Klines from Binance (spot or futures)
 
-        See dateparser docs for valid start and end string formats http://dateparser.readthedocs.io/en/latest/
+		See dateparser docs for valid start and end string formats http://dateparser.readthedocs.io/en/latest/
 
-        If using offset strings for dates add "UTC" to date string e.g. "now UTC", "11 hours ago UTC"
+		If using offset strings for dates add "UTC" to date string e.g. "now UTC", "11 hours ago UTC"
 
-        :param symbol: Name of symbol pair e.g BNBBTC
-        :type symbol: str
-        :param interval: Binance Kline interval
-        :type interval: str
-        :param start_str: Start date string in UTC format or timestamp in milliseconds
-        :type start_str: str|int
-        :param end_str: optional - end date string in UTC format or timestamp in milliseconds (default will fetch everything up to now)
-        :type end_str: None|str|int
-        :param limit: Default 500; max 1000.
-        :type limit: int
-        :param limit: Default 500; max 1000.
-        :type limit: int
-        :param klines_type: Historical klines type: SPOT or FUTURES
-        :type klines_type: HistoricalKlinesType
+		:param symbol: Name of symbol pair e.g BNBBTC
+		:type symbol: str
+		:param interval: Binance Kline interval
+		:type interval: str
+		:param start_str: Start date string in UTC format or timestamp in milliseconds
+		:type start_str: str|int
+		:param end_str: optional - end date string in UTC format or timestamp in milliseconds (default will fetch everything up to now)
+		:type end_str: None|str|int
+		:param limit: Default 500; max 1000.
+		:type limit: int
+		:param limit: Default 500; max 1000.
+		:type limit: int
+		:param klines_type: Historical klines type: SPOT or FUTURES
+		:type klines_type: HistoricalKlinesType
 
-        :return: list of OHLCV values
+		:return: list of OHLCV values
 
-        """
-        # init our list
-        output_data = []
+		"""
 
-        # convert interval to useful value in seconds
-        timeframe = interval_to_milliseconds(interval)
+		# convert interval to useful value in seconds
+		timeframe = interval_to_milliseconds(interval)
 
-        start_ts = convert_ts_str(start_str)
+		start_ts = convert_ts_str(start_str)
 
-        # establish first available start timestamp
-        first_valid_ts = self._get_earliest_valid_timestamp(symbol, interval, klines_type)
-        start_ts = max(start_ts, first_valid_ts)
+		# establish first available start timestamp
+		first_valid_ts = self._get_earliest_valid_timestamp(symbol, interval, klines_type)
+		start_ts = max(start_ts, first_valid_ts)
 
-        # if an end time was passed convert it
-        end_ts = convert_ts_str(end_str)
+		# if an end time was passed convert it
+		end_ts = convert_ts_str(end_str)
 
-        idx = 0
-        while True:
-            # fetch the klines from start_ts up to max 500 entries or the end_ts if set
-            temp_data = self._klines(
+	    i = 0
+        candles = []
+        limit_temp = limit
+
+        while len(candles) < limit:
+            candles_temp = self._klines(
                 klines_type=klines_type,
                 symbol=symbol,
                 interval=interval,
-                limit=limit,
+                limit=limit_temp,
                 startTime=start_ts,
                 endTime=end_ts
             )
 
-            # handle the case where exactly the limit amount of data was returned last loop
-            if not len(temp_data):
-                break
+            candles += candles_temp
 
-            # append this loops data to our output data
-            output_data += temp_data
+            limit_temp -= len(candles_temp)
 
-            # set our start timestamp using the last value in the array
-            start_ts = temp_data[-1][0]
-
-            idx += 1
-            # check if we received less than the required limit and exit the loop
-            if len(temp_data) < limit:
-                # exit the while loop
-                break
-
-            # increment next call by our timeframe
-            start_ts += timeframe
+            last_ts = candles[-1][0]
+            start_ts = last_ts + timeframe
 
             # sleep after every 3rd call to be kind to the API
-            if idx % 3 == 0:
+            i += 1
+            if i % 3 == 0:
                 time.sleep(1)
 
-        return output_data
+        return candles
 
     def get_historical_klines_generator(self, symbol, interval, start_str, end_str=None,
                                         klines_type: HistoricalKlinesType = HistoricalKlinesType.SPOT):
